8fd78698f3a5a3f89cc184223629da7bce4872f4
royletron <darren@teachertapp.co.uk>
2025-04-20 18:14:46 +0100

    Tweak photos

--- src/components/builder/components/PhotoBooth.tsx
+++ src/components/builder/components/PhotoBooth.tsx
@@ -26,6 +26,8 @@ interface PhotoControlsProps {
   onTakeScreenshot: () => void;
   aspectRatio: AspectRatio;
   setAspectRatio: (aspectRatio: AspectRatio) => void;
+  dark: boolean;
+  setDark: (dark: boolean) => void;
 }
 
 export enum AspectRatio {
@@ -37,10 +39,11 @@ function PhotoControls({
   onTakeScreenshot,
   aspectRatio,
   setAspectRatio,
+  dark,
+  setDark,
 }: PhotoControlsProps) {
   const { zoomIn, zoomOut } = useControls();
   const { rotation, setRotation } = useRotation();
-
   const [scale, setScale] = useState(0.7);
 
   const updateScale = (e) => {
@@ -61,7 +64,7 @@ function PhotoControls({
   });
 
   return (
-    <div className=" flex flex-row justify-center gap-4 w-full py-4 ">
+    <div className="flex flex-row justify-center items-center gap-4 w-full py-4 ">
       <div className="flex-1 flex justify-end gap-2">
         <div className="flex flex-col gap-0.5 flex-1 items-end">
           <label className="label">Zoom</label>
@@ -74,25 +77,16 @@ function PhotoControls({
             value={scale}
             onChange={updateScale}
           />
-        </div>
-      </div>
-      <div className="flex flex-col items-center gap-2">
-        <a
-          className="btn rounded-full btn-xl btn-square btn-error"
-          onClick={onTakeScreenshot}
-        ></a>
-        <div className="flex flex-col gap-0.5 justify-end">
-          <label className="label"></label>
-          <a
-            className="btn btn-square btn-sm"
-            onClick={() => {
-              setAspectRatio(
-                aspectRatio === AspectRatio.Square
-                  ? AspectRatio.Portrait
-                  : AspectRatio.Square
-              );
-            }}
-          >
+          <label className="toggle text-base-content mt-2">
+            <input
+              type="checkbox"
+              checked={aspectRatio === AspectRatio.Square}
+              onChange={(e) => {
+                setAspectRatio(
+                  e.target.checked ? AspectRatio.Square : AspectRatio.Portrait
+                );
+              }}
+            />
             <svg
               viewBox="0 0 24 24"
               fill="none"
@@ -105,8 +99,25 @@ function PhotoControls({
               <rect width="12" height="20" x="6" y="2" rx="2" />
               <rect width="20" height="12" x="2" y="6" rx="2" />
             </svg>
-          </a>
+            <svg
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="size-4"
+            >
+              <rect width="18" height="18" x="3" y="3" rx="2" />
+            </svg>
+          </label>
         </div>
+      </div>
+      <div className="flex flex-col items-center gap-2">
+        <a
+          className="btn rounded-full btn-xl btn-square btn-error shutter"
+          onClick={onTakeScreenshot}
+        ></a>
       </div>
       <div className="flex-1 flex">
         <div className="flex flex-col gap-0.5 flex-1">
@@ -121,6 +132,47 @@ function PhotoControls({
               setRotation(parseInt(e.target.value));
             }}
           />
+          <label className="toggle text-base-content mt-2">
+            <input
+              type="checkbox"
+              checked={dark}
+              onChange={(e) => {
+                setDark(e.target.checked);
+              }}
+            />
+            <svg
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="size-4"
+            >
+              <circle cx="12" cy="12" r="4" />
+              <path d="M12 2v2" />
+              <path d="M12 20v2" />
+              <path d="m4.93 4.93 1.41 1.41" />
+              <path d="m17.66 17.66 1.41 1.41" />
+              <path d="M2 12h2" />
+              <path d="M20 12h2" />
+              <path d="m6.34 17.66-1.41 1.41" />
+              <path d="m19.07 4.93-1.41 1.41" />
+            </svg>
+            <svg
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="size-4"
+            >
+              <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9" />
+              <path d="M20 3v4" />
+              <path d="M22 5h-4" />
+            </svg>
+          </label>
         </div>
       </div>
     </div>
@@ -165,6 +217,7 @@ export default function PhotoBooth({ open, onClose }: PhotoBoothProps) {
   const [aspectRatio, setAspectRatio] = useState<AspectRatio>(
     AspectRatio.Square
   );
+  const [dark, setDark] = useState(false);
 
   const ref = useRef<HTMLDivElement>(null);
 
@@ -211,17 +264,10 @@ export default function PhotoBooth({ open, onClose }: PhotoBoothProps) {
       });
   };
 
-  const { instance } = useControls();
   return (
     <animated.div className="fixed inset-0 z-10" style={styles}>
       <Flashbulb run={screen} />
       <TransformWrapper minScale={0.5} maxScale={5}>
-        {/* <Passthru
-          open={open}
-          scale={instance.transformState.scale}
-          positionX={instance.transformState.positionX}
-          positionY={instance.transformState.positionY}
-        /> */}
         <div className="inset-0 absolute bg-neutral-100 z-10 p-4 flex flex-col">
           <a
             className="absolute btn btn-square top-4 right-4 z-20"
@@ -248,7 +294,7 @@ export default function PhotoBooth({ open, onClose }: PhotoBoothProps) {
           >
             <div
               className={classes(
-                "bg-amber-950 m-auto",
+                "m-auto",
                 classIf(aspectRatio == AspectRatio.Portrait, "aspect-[2/3]"),
                 classIf(aspectRatio == AspectRatio.Square, "aspect-square")
               )}
@@ -268,7 +314,11 @@ export default function PhotoBooth({ open, onClose }: PhotoBoothProps) {
               >
                 <div
                   ref={ref}
-                  className="absolute inset-0 overflow-hidden bg-white"
+                  className={classes(
+                    "absolute inset-0 overflow-hidden transition-colors",
+                    classIf(dark, "bg-neutral-900"),
+                    classIf(!dark, "bg-white")
+                  )}
                 >
                   <TransformComponent
                     wrapperStyle={{
@@ -282,7 +332,10 @@ export default function PhotoBooth({ open, onClose }: PhotoBoothProps) {
                   >
                     <Raw />
                   </TransformComponent>
-                  <Foreground aspectRatio={aspectRatio} />
+                  <Foreground
+                    aspectRatio={aspectRatio}
+                    color={dark ? "white" : "black"}
+                  />
                 </div>
               </div>
             </div>
@@ -291,6 +344,8 @@ export default function PhotoBooth({ open, onClose }: PhotoBoothProps) {
             onTakeScreenshot={takeScreenshot}
             aspectRatio={aspectRatio}
             setAspectRatio={setAspectRatio}
+            dark={dark}
+            setDark={setDark}
           />
         </div>
       </TransformWrapper>
